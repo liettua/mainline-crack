184b9fb55d42bf71d2d31ed59420551ebee5c0c6
yEsPaP <csgo40055@gmail.com>
2022-05-28 06:12:02 +0000

    use custom clang

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export PATH="$HOME/clang-14.0.4/bin/:$PATH"
+
 make LLVM=1 LLVM_IAS=1 KCPPFLAGS="-mcpu=native -march=native -mtune=native" KAFLAGS="-mcpu=native -march=native -mtune=native" KCFLAGS="-mcpu=native -march=native -mtune=native" config_defconfig
 make LLVM=1 LLVM_IAS=1 KCPPFLAGS="-mcpu=native -march=native -mtune=native" KAFLAGS="-mcpu=native -march=native -mtune=native" KCFLAGS="-mcpu=native -march=native -mtune=native" -j$(nproc --all) deb-pkg
